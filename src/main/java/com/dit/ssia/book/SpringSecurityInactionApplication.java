5a6caf07502de7e199ddd896e84cd878525c379d
Daniel Xu <zhiqiang.xu2015@gmail.com>
2023-03-05 23:47:08 -0500

    init commit

---
+++ src/main/java/com/dit/ssia/book/SpringSecurityInactionApplication.java
@@ -0,0 +1,13 @@
+package com.dit.ssia.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dit/ssia/book/SpringSecurityInactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.ssia.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
